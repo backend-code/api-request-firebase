345906f6b0979056ef846888690ef80c2ada121d
Nutthapat Piasangka <n.devs260340@gmail.com>
2020-02-03 21:05:24 +0700

    update

--- index.js
+++ index.js
@@ -3,29 +3,41 @@ import React from 'react';
 
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
+const ApiResponseJson = require('api-response-json')
 
 export const POST = function (url, data) {
-    fetch(`${url}`, {
+   return fetch(`${url}`, {
         mode: 'no-cors',
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
+    }).then((d) => {
+        ApiResponseJson.created("create ok!",201, d)
+    }).catch((err) => {
+        ApiResponseJson.error('create error', err)
     });
 }
 
 export const PUT = function (url, data) {
-    fetch(`${url}`, {
+  return fetch(`${url}`, {
         mode: 'no-cors',
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
+    }).then((d) => {
+        ApiResponseJson.ok("update ok!",200, d)
+    }).catch((err) => {
+        ApiResponseJson.error('update error', err)
     });
 }
 
 export const GET = function (url) {
     return fetch(`${url}`).then(
         function (res) {
+            ApiResponseJson.ok("get ok!",200, res)
             return res.json();
         }
-    )
+    ).catch((err) => {
+        ApiResponseJson.error('get error', err)
+    });
 }
