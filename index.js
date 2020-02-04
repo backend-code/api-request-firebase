002d6688d0f3a603791a96280cc9792675e6a187
Nutthapat Piasangka <n.devs260340@gmail.com>
2020-02-04 19:53:22 +0700

    update

--- index.js
+++ index.js
@@ -5,39 +5,45 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 const ApiResponseJson = require('api-response-json')
 
-export const POST = function (url, data) {
-   return fetch(`${url}`, {
+export async function POST (url, data) {
+   
+    const response = await fetch(`${url}`, {
         mode: 'no-cors',
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
-    }).then((d) => {
-        ApiResponseJson.created("create ok!",201, d)
-    }).catch((err) => {
-        ApiResponseJson.error('create error', err)
-    });
+    })
+
+    return await response;
 }
 
-export const PUT = function (url, data) {
-  return fetch(`${url}`, {
-        mode: 'no-cors',
+export async function PUT (url, data) {
+    const response = await fetch(`${url}`, {
+        mode: 'cors',
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
-    }).then((d) => {
-        ApiResponseJson.ok("update ok!",200, d)
-    }).catch((err) => {
-        ApiResponseJson.error('update error', err)
-    });
+    })
+
+    return await response;
 }
 
-export const GET = function (url) {
-    return fetch(`${url}`).then(
+export async function GET (url) {
+    const response = await fetch(`${url}`).then(
         function (res) {
-            ApiResponseJson.ok("get ok!",200, res)
+            // return ApiResponseJson.ok("get ok!", 200, res)
             return res.json();
         }
-    ).catch((err) => {
-        ApiResponseJson.error('get error', err)
-    });
+    );
+
+    return await response;
+}
+
+export async function REMOVE(url) {
+    const response = await fetch(`${url}`, {
+        mode: 'no-cors',
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    return await response;
 }
